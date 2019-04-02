1d6827570d4946e860db2d1df190aa787dd90775
AJ <38798090+axjasf@users.noreply.github.com>
2019-04-02 16:50:33 -0700

    Create find_force_of_gravity_between_two_objects.py
    Need to find a better way to convert the units

---
+++ Python/8kyu/find_force_of_gravity_between_two_objects.py
@@ -0,0 +1,45 @@
+# Find force of gravity between two objects
+# Level 8kyu
+
+def convert_mass_to_kg(mass, unit) :
+    mass = float(mass)
+    unit = str(unit)
+    
+    if unit == "g" :
+        factor = 0.001
+    elif unit == "mg" :
+        factor = 1 * (10**-6)
+    elif unit == "μg" :
+        factor = 1 * (10**-9)
+    elif unit == "lb" :
+        factor = 0.453592
+    else : 
+        factor = 1
+    return float(mass * factor)
+
+def convert_distance_to_meters(distance, unit) :
+    distance = float(distance)
+    unit = str(unit)
+    
+    if unit == "cm" :
+        factor = 0.01
+    elif unit == "mm" :
+        factor = 0.001
+    elif unit == "μm" :
+        factor = 1 * (10**-6)
+    elif unit == "ft" :
+        factor = 0.3048
+    else : 
+        factor = 1
+    return float(distance * factor)
+
+def solution(arr_val, arr_unit) :
+    # Define gravity G
+    G = 6.67*(10**-11)
+    
+    m1 = convert_mass_to_kg(arr_val[0], arr_unit[0])
+    m2 = convert_mass_to_kg(arr_val[1], arr_unit[1])
+    d = convert_distance_to_meters(arr_val[2], arr_unit[2])
+    
+    F_grav = (G * m1 * m2) / (d**2)
+    return F_grav

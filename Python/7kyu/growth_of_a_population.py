069cd7e693fc9613aedcccf2452be37fec39ccfb
AJ <38798090+axjasf@users.noreply.github.com>
2019-04-03 13:44:59 -0700

    Update growth_of_a_population.py

--- Python/7kyu/growth_of_a_population.py
+++ Python/7kyu/growth_of_a_population.py
@@ -3,7 +3,7 @@
 # https://www.codewars.com/kata/reviews/563b677259afc2b5120000cd/groups/5ca51a2800f2270001f1b0f2
 #
 # Variables:
-#   Start population (*p0*), Growth per year in percentage (*percent*), additional augmented growth per year (*aug*), final population (*p*
+#   Start population (p0), Growth per year in percentage (percent), additional augmented growth per year (aug), final population (p)
 
 def nb_year(p0, percent, aug, p):
     

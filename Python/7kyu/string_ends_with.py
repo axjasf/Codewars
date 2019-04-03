f604133bbc5e0eadc4f52f13f579220c47e0d143
AJ <38798090+axjasf@users.noreply.github.com>
2019-04-02 17:45:45 -0700

    Update string_ends_with.py

--- Python/7kyu/string_ends_with.py
+++ Python/7kyu/string_ends_with.py
@@ -1,6 +1,6 @@
 # Find out, if a string ends with a specific ending
 # Level: 7kyu
-# https://www.codewars.com/kata/reviews/5463ab67541c67d9b0000064/groups/5463ab67541c67d9b0000066 
+# https://www.codewars.com/kata/reviews/5463ab67541c67d9b0000064/groups/546439056be3378e7a000367
 
 def solution(string, ending):
     return string[-len(ending):] == ending
